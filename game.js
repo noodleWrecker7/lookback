60b22e9ad1d96aeb767549c7ea3df4a0ccaa23f5
noodleWrecker7 <hodgkinson.adam@gmail.com>
2019-12-21 10:02:37 +0000

    add copyright

--- game.js
+++ game.js
@@ -1,8 +1,15 @@
-/*******************************************************************************
+/*
  * Copyright (c) 2019.
  * Developed by Adam Hodgkinson
- * Last modified 15/11/2019, 18:06
- ******************************************************************************/
+ * Last modified 21/12/12 10:2
+ *
+ * Everything on this page, and other pages on the website, is subject to the copyright of Adam Hodgkinson, it may be freely used, copied, distributed and/or modified, however, full credit must be given
+ * to me and any derived works should be released under the same license. I am not held liable for any claim, this software is provided as-is and without any warranty.
+ *
+ * I do not own any of the following content and is used under their respective licenses:
+ *     Fontawesome
+ *     Photonstorm's phaser.js
+ */
 
 var debug = false;
 
