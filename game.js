f943a1d607bb707a401bac6e8e197e9608cccd3e
noodleWrecker7 <hodgkinson.adam@gmail.com>
2020-03-14 20:27:15 +0000

    Will check if the current session already exists, and will try to use it

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2020.
  * Developed by Adam Hodgkinson
- * Last modified 31/1/1 21:23
+ * Last modified 14/3/3 20:27
  *
  * Everything on this page, and other pages on the website, is subject to the copyright of Adam Hodgkinson, it may be freely used, copied, distributed and/or modified, however, full credit must be given
  * to me and any derived works should be released under the same license. I am not held liable for any claim, this software is provided as-is and without any warranty.
